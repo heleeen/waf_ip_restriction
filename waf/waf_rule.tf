9c69446a27be42004eea4944f762c96e3674d6f8
heleeen <nishikei.0116@gmail.com>
2018-10-05 11:41:12 +0900

    remove Roppongi

--- waf/waf_rule.tf
+++ waf/waf_rule.tf
@@ -1,6 +1,6 @@
 resource "aws_waf_rule" "allow_ip_address_rule" {
   name        = "${ var.prefix }-allow-ip-address-rule"
-  metric_name = "AllowRoppongiIpRule"
+  metric_name = "AllowIpRule"
 
   predicates {
     data_id = "${ aws_waf_ipset.allow_ipset.id }"
